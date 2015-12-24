5c8da5ab5a7c7f825af05a442b2fabf188a6ce22
Trevor Strobel <trevorstrobel@gmail.com>
2015-12-24 16:14:04 -0500

    adding calculator project

---
+++ functions/calculator/calc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 5;
+	int y;
+	cout << "enter a number please" + endl;
+	cin >> y;
+	int answer = x+y;
+	cout << answer << endl;
+	
+	return 0;
+}
